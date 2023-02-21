592d189a83fe0a19c3a55059ba9226c0f81bf124
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-02-21 20:49:37 +0530

    1 file modified

--- src/pages/PostLogin/GamePlayer.js
+++ src/pages/PostLogin/GamePlayer.js
@@ -2,7 +2,10 @@ import React from 'react';
 export default function GamePlayer({ game }) {
   const Sidebar = () => {
     return (
-      <div className="sidebar">
+      <div
+        className="sidebar"
+        style={{ flex: 1, borderRight: '1px Solid #d6d6d6', marginRight: 20 }}
+      >
         <ul>
           <li>
             <a href="./">Home</a>
@@ -21,10 +24,10 @@ export default function GamePlayer({ game }) {
     );
   };
   const Player = () => {
-    return <div>Player IFrame:{game}</div>;
+    return <div style={{ flex: 2 }}>Player IFrame:{game}</div>;
   };
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'space-arround' }}>
       <Sidebar />
       {/* iframe here */}
       <Player />
